dddb4b59eba3164ebf96bb815c1fb8128c741cb3
Diego <diegohr.lr@gmail.com>
2017-12-18 12:53:10 -0200

    First Commit

---
+++ Fdv1/DriverController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace Fdv1
+{
+    class DriverController
+    {
+        public static IWebDriver driver { get; set; }
+
+
+        public static void enterTextByName(string element,string text)
+        {
+            driver.FindElement(By.Name(element)).SendKeys(text);
+        }
+
+        public static void enterTextById(string element, string text)
+        {
+            driver.FindElement(By.Id(element)).SendKeys(text);
+            
+        }
+
+
+
+    }
+}
---
+++ Fdv1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace Fdv1
+{
+
+    class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            //Config the ChromeDriver
+            DriverController.driver = new ChromeDriver();
+            DriverController.driver.Manage().Window.Maximize();
+            DriverController.driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            DriverController.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            //Navigate to Google Page
+            DriverController.driver.Navigate().GoToUrl("http://www.google.com");
+            Console.WriteLine("Opened URL");
+
+            //Search Seleniumhq and enter into the Seleniumhq Site
+            DriverController.enterTextById("lst-ib", "Seleniumhq");
+            DriverController.driver.FindElement(By.Id("lst-ib")).SendKeys(Keys.Enter);
+            DriverController.driver.FindElement(By.XPath("//a[@href='http://www.seleniumhq.org/']")).Click();
+            Console.WriteLine("Executed TestCase1");
+
+            //Verify the seleniumhq url
+            Assert.That(DriverController.driver.Url == "http://www.seleniumhq.org/");
+            Console.WriteLine("Assert executed");
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //Close the driver
+            DriverController.driver.Close();
+            Console.WriteLine("Driver closed");
+        }
+
+
+
+
+    }
+}
